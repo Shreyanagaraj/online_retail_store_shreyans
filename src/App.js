36dac1b52d57c1d5c1c378488bef4924b13ab484
Shreyanagaraj <shreyansshreyans73@gmail.com>
2024-09-24 15:25:58 +0530

    first set of files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Product from './components/Product'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome</h1>
+      <p>This is react component</p>
+      </header>
+      <Category />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
